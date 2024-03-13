cbd696f5a4c372a461cfff1f625be9a89d569184
Ivan Suhendra <38711246+van-grafity@users.noreply.github.com>
2024-03-13 10:26:53 +0700

    added callback

--- resources/views/pages/brands/index.blade.php
+++ resources/views/pages/brands/index.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title mb-0">Brands</h3>
                         <div class="card-tools d-flex">
-                            <button type="button" class="btn btn-primary btn-sm" onclick="showModal(true)">Add Brand</button>
                         </div>
                     </div>
                     <div class="card-body">
@@ -61,84 +60,9 @@
 
 @push('js')
 <script>
-    function showModal(add, id = null) {
-        $modal = $('#modal_brand');
-        $form = $('#form_brand');
-        $title = $('#modal_title');
-        $btn = $('#modal_btn');
-        $name = $('#name');
-
-        if (add) {
-            $form.attr('action', '{{ route('brands.store') }}');
-            $form.trigger('reset');
-            $title.text('Add Brand');
-            $btn.text('Save');
-        } else {
-            $form.append('<input type="hidden" name="_method" value="PUT">');
-            $form.attr('action', '{{ route('brands.update', '') }}/' + id);
-            $title.text('Edit Brand');
-            $btn.text('Update');
-
-            $.get('{{ route('brands.show', '') }}/' + id, function(data) {
-                $name.val(data.name);
-            });
-        }
-
-        $modal.modal('show');
-    }
-
-    function deleteData(id) {
-        confirmAction("Are you sure?", "You won't be able to revert this!", "warning", function() {
-            $.ajax({
-                url: '{{ route('brands.destroy', '') }}/' + id,
-                type: 'POST',
-                data: {
-                    '_method': 'DELETE',
-                    '_token': '{{ csrf_token() }}'
-                },
-                success: function(data) {
-                    showAlert("Deleted!", "Your file has been deleted.", "success", function() {
-                        location.reload();
-                    });
-                },
-                error: function(data) {
-                    showAlert("Error!", "There is an error deleting the data.", "error");
-                }
-            });
-        });
-    }
-
-    function confirmAction(title, text, icon, callback) {
-        Swal.fire({
-            title: title,
-            text: text,
-            icon: icon,
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.value) {
-                callback();
-            }
-        });
-    }
-
-    function showAlert(title, text, icon, callback = null) {
-        Swal.fire({
-            title: title,
-            text: text,
-            icon: icon,
-            confirmButtonColor: '#3085d6',
-        }).then((result) => {
-            if (callback && result.value) {
-                callback();
-            }
-        });
-    }
-
-    $(function() {
-        $('#brands_table').DataTable({
+    $(document).ready(function() {
+        // DataTable initialization
+        var table = $('#brands_table').DataTable({
             processing: true,
             serverSide: true,
             ajax: '{{ route('brands.index') }}',
@@ -148,17 +72,117 @@
                 { data: 'created_at', name: 'created_at' },
                 { data: 'updated_at', name: 'updated_at' },
                 { 
-                    data: 'action',
-                    name: 'action',
-                    orderable: false,
-                    searchable: false,
+                    data: null,
                     render: function(data, type, full, meta) {
-                        return `<a href="#" class="btn btn-info btn-sm" onclick="showModal(false, ${full.id})">Detail</a>
-                                <a href="javascript:void(0)" class="btn btn-danger btn-sm" onclick="deleteData(${full.id})">Delete</a>`;
+                        return `<button type="button" class="btn btn-info btn-sm btn-detail" data-id="${full.id}">Detail</button>
+                                <button type="button" class="btn btn-danger btn-sm btn-delete" data-id="${full.id}">Delete</button>`;
                     }
                 }
-            ]
+            ],
+            order: [[0, 'asc']],
+            lengthChange: true,
+            searching: true,
+            autoWidth: false,
+            responsive: true
         });
+        
+        // Show modal for add
+        $('.card-tools').append('<button type="button" class="btn btn-primary" id="btn_add">Add Brand</button>');
+        $('#btn_add').on('click', function() {
+            showModal(true);
+        });
+
+        // Show modal for detail
+        $('#brands_table').on('click', '.btn-detail', function() {
+            var id = $(this).data('id');
+            showModal(false, id);
+        });
+
+        // Delete brand
+        $('#brands_table').on('click', '.btn-delete', function() {
+            var id = $(this).data('id');
+            deleteData(id);
+        });
+
+        // Function to show modal
+        function showModal(add, id = null) {
+            var $modal = $('#modal_brand');
+            var $form = $('#form_brand');
+            var $title = $('#modal_title');
+            var $btn = $('#modal_btn');
+            var $name = $('#name');
+
+            if (add) {
+                $form.attr('action', '{{ route('brands.store') }}');
+                $form.trigger('reset');
+                $title.text('Add Brand');
+                $btn.text('Save');
+            } else {
+                $form.append('<input type="hidden" name="_method" value="PUT">');
+                $form.attr('action', '{{ route('brands.update', '') }}/' + id);
+                $title.text('Edit Brand');
+                $btn.text('Update');
+
+                $.get('{{ route('brands.show', '') }}/' + id, function(data) {
+                    $name.val(data.name);
+                });
+            }
+
+            $modal.modal('show');
+        }
+
+        // Function to delete data
+        function deleteData(id) {
+            confirmAction("Are you sure?", "You won't be able to revert this!", "warning", function() {
+                $.ajax({
+                    url: '{{ route('brands.destroy', '') }}/' + id,
+                    type: 'POST',
+                    data: {
+                        '_method': 'DELETE',
+                        '_token': '{{ csrf_token() }}'
+                    },
+                    success: function(data) {
+                        showAlert("Deleted!", "Your file has been deleted.", "success", function() {
+                            table.ajax.reload();
+                        });
+                    },
+                    error: function(data) {
+                        showAlert("Error!", "There is an error deleting the data.", "error");
+                    }
+                });
+            });
+        }
+
+        // Function to confirm action
+        function confirmAction(title, text, icon, callback) {
+            Swal.fire({
+                title: title,
+                text: text,
+                icon: icon,
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.value) {
+                    callback();
+                }
+            });
+        }
+
+        // Function to show alert
+        function showAlert(title, text, icon, callback = null) {
+            Swal.fire({
+                title: title,
+                text: text,
+                icon: icon,
+                confirmButtonColor: '#3085d6',
+            }).then((result) => {
+                if (callback && result.value) {
+                    callback();
+                }
+            });
+        }
     });
 </script>
 @endpush
